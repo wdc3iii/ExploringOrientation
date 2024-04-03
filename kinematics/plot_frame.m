3ca7a0091da61a91e4cda6bc644fd821f2f4c57d
Will Compton <wdc3iii@gmail.com>
2024-04-03 15:56:31 -0700

    Rot and quat

---
+++ kinematics/plot_frame.m
@@ -0,0 +1,18 @@
+function plot_frame(fig, pos, rot, rep, label)
+figure(fig);
+hold on
+if strcmp(rep, 'quat')
+    rot = quat2rotm(rot);
+end
+endpts = rot * 0.2 + pos;
+n = [nan; nan; nan];
+lines = [pos endpts(:, 1) n pos endpts(:, 2) n pos endpts(:, 3)];
+plot3(lines(1, :), lines(2, :), lines(3, :), 'k','HandleVisibility','off')
+plot3(endpts(1, :), endpts(2, :), endpts(3, :), '.', 'MarkerSize', 20, 'DisplayName', label)
+legend('Location', 'northwest')
+xlabel('X')
+ylabel('Y')
+zlabel('Z')
+view([45, 45])
+axis square
+end
